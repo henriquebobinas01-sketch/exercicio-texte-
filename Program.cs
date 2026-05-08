5fb726cd42b8aaa74f32caafec1ed79d1bddadf9
henrique oliveiro <henriquebobinas01@gmail.com>
2026-05-08 20:40:40 -0300

    salvooo

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("qual a sua idade:"); 
+int idade = Convert.ToInt32(Console.ReadLine());
+
+if (idade >= 18)
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine("voce é maior de idade ");
+}
+else
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("voce e menor de idade ");
+}
+
+Console.ResetColor();
+Console.WriteLine("obrigadooooo ");
